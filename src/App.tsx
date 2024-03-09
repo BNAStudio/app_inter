cb0e1ddea0ba3142d071f7bc148cac2d4302c8d7
Daniel Marcovich <danielmark999@gmail.com>
2024-03-09 12:39:08 -0500

    feat(root): add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import './App.css';
-import Dashboard from './components/Dashboard';
+import Dashboard from './pages/Dashboard';
+import { Cart } from './pages/Cart';
 
 function App() {
   return (
-    <div className="App">
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 

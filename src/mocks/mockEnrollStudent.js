b9438abc545b103932e0d36b67ebaed1876f2613
Daniel Marcovich <danielmark999@gmail.com>
2024-03-10 11:04:09 -0500

    feat(mocks): add mock for enrolled students

---
+++ src/mocks/mockEnrollStudent.js
@@ -0,0 +1,220 @@
+export const enrolledStudents = [
+    {
+        "courseId": 1,
+        "students": [
+            {
+                "id": 462,
+                "name": "Lucía",
+                "lastname": "Martínez",
+                "nationality": "Venezuela",
+                "credits": 3,
+                "img": "./media/img/alice_cat.jpg"
+            },
+            {
+                "id": 547,
+                "name": "Carlos",
+                "lastname": "Rodríguez",
+                "nationality": "Chile",
+                "credits": 3,
+                "img": "./media/img/alice_cat.jpg"
+            },
+        ]
+    },
+    {
+        "courseId": 2,
+        "students": [
+            {
+                "id": 53,
+                "name": "Luis",
+                "lastname": "Rodríguez",
+                "nationality": "Brasil",
+                "credits": 3,
+                "img": "./media/img/alice_cat.jpg"
+            },
+        ]
+    },
+    {
+        "courseId": 3,
+        "students": [
+            {
+                "id": 547,
+                "name": "Carlos",
+                "lastname": "Rodríguez",
+                "nationality": "Chile",
+                "credits": 3,
+                "img": "./media/img/alice_cat.jpg"
+            },
+            {
+                "id": 462,
+                "name": "Lucía",
+                "lastname": "Martínez",
+                "nationality": "Venezuela",
+                "credits": 3,
+                "img": "./media/img/alice_cat.jpg"
+            }
+        ]
+    },
+    {
+        "courseId": 4,
+        "students": [
+            {
+                "id": 921,
+                "name": "Sofia",
+                "lastname": "García",
+                "nationality": "Perú",
+                "credits": 3,
+                "img": "./media/img/alice_cat.jpg"
+            },
+            {
+                "id": 547,
+                "name": "Carlos",
+                "lastname": "Rodríguez",
+                "nationality": "Chile",
+                "credits": 3,
+                "img": "./media/img/alice_cat.jpg"
+            },
+            {
+                "id": 462,
+                "name": "Lucía",
+                "lastname": "Martínez",
+                "nationality": "Venezuela",
+                "credits": 3,
+                "img": "./media/img/alice_cat.jpg"
+            },
+        ]
+    },
+    {
+        "courseId": 5,
+        "students": [
+            {
+                "id": 462,
+                "name": "Lucía",
+                "lastname": "Martínez",
+                "nationality": "Venezuela",
+                "credits": 3,
+                "img": "./media/img/alice_cat.jpg"
+            },
+            {
+                "id": 53,
+                "name": "Luis",
+                "lastname": "Rodríguez",
+                "nationality": "Brasil",
+                "credits": 3,
+                "img": "./media/img/alice_cat.jpg"
+            },
+        ]
+    },
+    {
+        "courseId": 6,
+        "students": [
+            {
+                "id": 53,
+                "name": "Luis",
+                "lastname": "Rodríguez",
+                "nationality": "Brasil",
+                "credits": 3,
+                "img": "./media/img/alice_cat.jpg"
+            },
+            {
+                "id": 462,
+                "name": "Lucía",
+                "lastname": "Martínez",
+                "nationality": "Venezuela",
+                "credits": 3,
+                "img": "./media/img/alice_cat.jpg"
+            },
+        ]
+    },
+    {
+        "courseId": 7,
+        "students": [
+            {
+                "id": 728,
+                "name": "Miguel",
+                "lastname": "Vázquez",
+                "nationality": "Uruguay",
+                "credits": 3,
+                "img": "./media/img/alice_cat.jpg"
+            },
+            {
+                "id": 921,
+                "name": "Sofia",
+                "lastname": "García",
+                "nationality": "Perú",
+                "credits": 3,
+                "img": "./media/img/alice_cat.jpg"
+            },
+            {
+                "id": 66,
+                "name": "Ana",
+                "lastname": "Vázquez",
+                "nationality": "Perú",
+                "credits": 3,
+                "img": "./media/img/alice_cat.jpg"
+            },
+        ]
+    },
+    {
+        "courseId": 8,
+        "students": [
+            {
+                "id": 84,
+                "name": "Sofia",
+                "lastname": "Sánchez",
+                "nationality": "Argentina",
+                "credits": 3,
+                "img": "./media/img/alice_cat.jpg"
+            },
+            {
+                "id": 66,
+                "name": "Ana",
+                "lastname": "Vázquez",
+                "nationality": "Perú",
+                "credits": 3,
+                "img": "./media/img/alice_cat.jpg"
+            },
+        ]
+    },
+    {
+        "courseId": 9,
+        "students": [
+            {
+                "id": 855,
+                "name": "Ana",
+                "lastname": "González",
+                "nationality": "Colombia",
+                "credits": 3,
+                "img": "./media/img/alice_cat.jpg"
+            },
+            {
+                "id": 921,
+                "name": "Sofia",
+                "lastname": "García",
+                "nationality": "Perú",
+                "credits": 3,
+                "img": "./media/img/alice_cat.jpg"
+            },
+        ]
+    },
+    {
+        "courseId": 10,
+        "students": [
+            {
+                "id": 66,
+                "name": "Ana",
+                "lastname": "Vázquez",
+                "nationality": "Perú",
+                "credits": 3,
+                "img": "./media/img/alice_cat.jpg"
+            },
+            {
+                "id": 462,
+                "name": "Lucía",
+                "lastname": "Martínez",
+                "nationality": "Venezuela",
+                "credits": 3,
+                "img": "./media/img/alice_cat.jpg"
+            },
+        ]
+    }
+]

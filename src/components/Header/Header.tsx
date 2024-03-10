15746f3fd260e31a2da88980a6c781346298b698
Daniel Marcovich <danielmark999@gmail.com>
2024-03-09 20:15:01 -0500

    refactor(styles): details styles update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,37 @@
-import './Header.scss'
+import { useState } from "react";
+import { user } from "../../mocks/mockUser";
 import { getFormattedDate } from "../../utils/utilDate";
+import './Header.scss'
+
 
 const Header = () => {
+    const [selectedCurrency, setSelectedCurrency] = useState('USD');
 
     const handleClick = (currency: string) => {
-        console.log("from header click", currency)
-    }
+        console.log("from header click", currency);
+        setSelectedCurrency(currency);
+    };
 
     return (
-            <div className='c-info-user'>
-                <p className='user-name'>User-Name</p>
-                <p className='current-date'>{getFormattedDate()}</p>
-                <div className='c-badge'>
-                    <button onClick={() => handleClick("USD")}>USD</button>
-                    <button onClick={() => handleClick("EUR")}>EUR</button>
-                </div>
-
+        <div className='c-info-user'>
+            <p className='user-name'>Welcome {user.name}!</p>
+            <p className='current-date'>{getFormattedDate()}</p>
+            <div className='c-badge'>
+                <button
+                    onClick={() => handleClick("USD")}
+                    className={selectedCurrency === "USD" ? 'selected' : ''}
+                >
+                    USD
+                </button>
+                <button
+                    onClick={() => handleClick("EUR")}
+                    className={selectedCurrency === "EUR" ? 'selected' : ''}
+                >
+                    EUR
+                </button>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default Header;

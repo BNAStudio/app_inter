10a324fe2c64fdb9952397ed8430c3a74bbc2390
Daniel Marcovich <danielmark999@gmail.com>
2024-03-10 13:47:38 -0500

    feat(root): add mocks copy to public folder

---
+++ public/mocks/mockCourses.js
@@ -0,0 +1,12 @@
+export const coursesInfo = [
+    { id: 1, name: "Materia 1", teacher: "Profesor A", credits: 3, price: 150, isSelected: false },
+    { id: 2, name: "Materia 2", teacher: "Profesor A", credits: 3, price: 150, isSelected: false },
+    { id: 3, name: "Materia 3", teacher: "Profesor B", credits: 3, price: 150, isSelected: false },
+    { id: 4, name: "Materia 4", teacher: "Profesor B", credits: 3, price: 150, isSelected: false },
+    { id: 5, name: "Materia 5", teacher: "Profesor C", credits: 3, price: 150, isSelected: false },
+    { id: 6, name: "Materia 6", teacher: "Profesor C", credits: 3, price: 150, isSelected: false },
+    { id: 7, name: "Materia 7", teacher: "Profesor D", credits: 3, price: 150, isSelected: false },
+    { id: 8, name: "Materia 8", teacher: "Profesor D", credits: 3, price: 150, isSelected: false },
+    { id: 9, name: "Materia 9", teacher: "Profesor E", credits: 3, price: 150, isSelected: false },
+    { id: 10, name: "Materia 10", teacher: "Profesor E", credits: 3, price: 150, isSelected: false }
+];
---
+++ public/mocks/mockUser.js
@@ -0,0 +1,10 @@
+export const userData = {
+    id: 12,
+    name: "User",
+    lastname: "Data",
+    img: "./media/img/alice_cat.jpg",
+    nationality: "Spain",
+    credits: 9,
+    courses: 0,
+    amount: 0
+}
